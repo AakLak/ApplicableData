5c1da58acc924ad2a0ce996be9f353e61ec0968a
Aakash Lakhotia <yohojo+dbc@gmail.com>
2016-09-02 12:13:44 -0700

    forgot a comma

--- app/importers/import_sale_csv.rb
+++ app/importers/import_sale_csv.rb
@@ -5,7 +5,7 @@ class ImportSaleCSV
 
   column :email, as: [/e.?mail/i], required: true
   column :order_date, as: ["date", "order_date"], required: true
-  column :amount, as:["price", "cost"] required: true
+  column :amount, as:["price", "cost"], required: true
 
   when_invalid :skip
 end
